6a8b4964dc5a28c65ec5bf6147f5ad3026deecfe
KY <55354225+okyspace@users.noreply.github.com>
2021-03-02 11:10:07 +0800

    bash script to start Triton Server

---
+++ triton/startserver_cpu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo starting triton with cpu mode
+
+sudo docker run --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p8000:8000 -p8001:8001 -p8002:8002 -v /media/ky/Data/workspace_aieng/dlmlops/usage-examples/triton/sample_model_repo:/models nvcr.io/nvidia/tritonserver:20.12-py3 tritonserver --model-repository=/models
