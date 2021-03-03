6e6ce8c9af1b0dcf329c51d15f671d8b1fe65107
KY <55354225+okyspace@users.noreply.github.com>
2021-03-04 00:15:06 +0800

    Updated start server scripts

--- triton/startserver_cpu.sh
+++ triton/startserver_cpu.sh
@@ -2,6 +2,4 @@
 
 echo starting triton with cpu mode
 
-# assume http port:8000
-# assume grpc port:8001
-sudo docker run --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p8000:8000 -p8001:8001 -p8002:8002 -v <path-to-model-repo>:/models nvcr.io/nvidia/tritonserver:20.12-py3 tritonserver --model-repository=/models
+docker run --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p8000:8000 -p8001:8001 -p8002:8002 -v /media/ky/Data/workspace_aieng/triton-server/model_repo:/models nvcr.io/nvidia/tritonserver:20.12-py3 tritonserver --model-repository=/models
---
+++ triton/startserver_gpu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo starting triton with gpu mode
+
+docker run --gpus all --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p8000:8000 -p8001:8001 -p8002:8002 -v /media/ky/Data/workspace_aieng/triton-server/model_repo:/models nvcr.io/nvidia/tritonserver:20.12-py3 tritonserver --model-repository=/models
